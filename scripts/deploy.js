d7aa0227b296126be2dfd41558f770d9f36bc989
NikitolProject <tolnikita556@gmail.com>
2023-05-22 23:49:39 +0300

    Small Changes

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,8 +2,8 @@ const hre = require("hardhat");
 
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
-  const usdtAddress = "0x55d398326f99059fF775485246999027B3197955"; // Замените на адрес вашего токена
-  const gbrAddress = "0xa970cae9fa1d7cca913b7c19df45bf33d55384a9"
+  const usdtAddress = "0x55d398326f99059fF775485246999027B3197955";
+  const gbrAddress = "0xa970cae9fa1d7cca913b7c19df45bf33d55384a9";
 
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
