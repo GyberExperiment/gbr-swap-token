1c2afec68d0c37121887cee6ff1fb4ef3ec90d92
NikitolProject <tolnikita556@gmail.com>
2023-05-25 00:27:19 +0300

    Add multiplication overflow protect to swap

--- contracts/SwapToken.sol
+++ contracts/SwapToken.sol
@@ -9,44 +9,48 @@ interface IERC20 {
 }
 
 contract GBRTokenSwap {
+    
     address public owner;
     IERC20 public usdtToken;
     IERC20 public gbrToken;
-    
+
     event Swap(address indexed user, uint256 amount);
-    
+
     modifier onlyOwner() {
         require(msg.sender == owner, "Only contract owner can call this function");
         _;
     }
-    
+
     constructor(address _usdtTokenAddress, address _gbrTokenAddress) {
         owner = msg.sender;
         usdtToken = IERC20(_usdtTokenAddress);
         gbrToken = IERC20(_gbrTokenAddress);
     }
-    
+
     function swap(uint256 _amount) external {
         require(_amount > 0, "Amount must be greater than zero");
-        
-        // Transfer USDT tokens from user to this contract
+        require(usdtToken.balanceOf(msg.sender) >= _amount, "Insufficient balance of USDT on your wallet");
+
         require(usdtToken.transferFrom(msg.sender, address(this), _amount), "Failed to transfer USDT tokens to contract");
-        
-        // Calculate the corresponding amount of GBR tokens based on a conversion rate
-        uint256 gbrAmount = _amount * 100; // 1 USDT = 100 GBR (adjust the conversion rate accordingly)
-        
-        // Transfer GBR tokens from this contract to the user
+
+        uint256 gbrAmount = _amount * 10000000; // 1 USDT = 10 000 000 GBR
+        require(gbrAmount / 10000000 == _amount, "SafeMath: multiplication overflow");
+
         require(gbrToken.transfer(msg.sender, gbrAmount), "Failed to transfer GBR tokens to user");
-        
+
         emit Swap(msg.sender, _amount);
     }
-    
+
     function withdrawTokens(address _tokenAddress, address _to, uint256 _amount) external onlyOwner {
         IERC20 token = IERC20(_tokenAddress);
         require(token.transfer(_to, _amount), "Failed to transfer tokens");
     }
-        
+
     function setOwner(address _newOwner) external onlyOwner {
         owner = _newOwner;
+    }
+
+    function getGBRTokenBalance() external view returns (uint256) {
+        return gbrToken.balanceOf(address(this));
     }
 }
